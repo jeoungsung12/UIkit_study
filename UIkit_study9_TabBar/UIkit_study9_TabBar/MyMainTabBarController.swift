38da0f50e285fcbcf71b42750660b765611fe3ba
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2023-07-20 22:31:42 +0900

    Add files via upload

---
+++ UIkit_study9_TabBar/UIkit_study9_TabBar/MyMainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkit_study9_TabBar
+//
+//  Created by 정성윤 on 2023/07/18.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
