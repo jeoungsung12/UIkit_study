cd1043f829a0e0bea7a8a732c2f8502d1029a271
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2023-07-20 22:30:20 +0900

    Add files via upload

---
+++ UIkit_study6_Extension/UIkit_study7_Extension/Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Extension.swift
+//  UIkit_study7_Extension
+//
+//  Created by 정성윤 on 2023/07/17.
+//
+
+import Foundation
+import UIKit
+extension UIViewController {
+    func sayHi() {
+        print("Hi")
+    }
+}
---
+++ UIkit_study6_Extension/UIkit_study7_Extension/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UIkit_study7_Extension
+//
+//  Created by 정성윤 on 2023/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        sayHi()
+    }
+}
+
