60cd152e72b79d4fd8c0bdb055881a63e1d0a8c7
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2023-07-20 22:31:16 +0900

    Add files via upload

---
+++ UIkit_study8_setting/UIkit_study8_setting/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UIkit_study8_setting
+//
+//  Created by 정성윤 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "뷰컨트롤러 No 스토리보드 - Swift"
+        self.view.backgroundColor = .orange
+    }
+
+
+}
+
