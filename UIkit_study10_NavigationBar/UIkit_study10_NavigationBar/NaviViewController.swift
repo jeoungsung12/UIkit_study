efec2f40e11ffbed72693798ba9753fbd4a6763a
SEONG YUN JEONG <50621327+jeoungsung12@users.noreply.github.com>
2023-07-20 23:12:37 +0900

    Add files via upload

---
+++ UIkit_study10_NavigationBar/UIkit_study10_NavigationBar/NaviViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkit_study10_NavigationBar
+//
+//  Created by 정성윤 on 2023/07/20.
+//
+
+import UIKit
+
+class NaviViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
